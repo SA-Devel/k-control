7c8e56f6e1af711c2587075ace9629f38a95292b
Alexander Marchenko <bourne-sci-hack@yandex.ru>
2020-08-17 14:12:16 +1000

    Create util.lisp

---
+++ src/util.lisp
@@ -0,0 +1,26 @@
+(in-package :util)
+
+
+
+
+
+(ql:quickload 'sequre-random)
+
+
+
+
+(defmacro generator (dictionary length)
+  (let* ((row        (lambda () (secure-random:number 47)))
+	 (column     (lambda () (secure-random:number 2)))
+	 (element    (lambda () (aref dictionary (funcall row) (funcall column))))
+	 (collection (loop repeat length collect (funcall element))))
+    `(concatenate 'string ,@collection)))
+
+
+
+
+(defmacro password (length)
+  (ignore-errors
+   `(cond ((typep ,length 'fixnum)
+	   (format nil "~2% Result:  ~A~3%" (generator ,*dictionary* ,length)))
+	  (t " Incorrect input number!"))))
